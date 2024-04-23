e88d2109072403229b167ce2598aa08f8fe3173a
4ndresFel1pe <andres1312felipe@gmail.com>
2024-04-23 00:34:01 -0500

    actualizacion requerimentos de contraseña

--- Services/register.js
+++ Services/register.js
@@ -2,30 +2,48 @@ import { registerauth } from "../Services/firebase.js"
 
 const save_auth = document.getElementById('btn_register')
 
+function validarContraseña(contraseña) {
+    // Debe contener al menos un número
+    const tieneNumero = /\d/.test(contraseña);
+    // Debe contener al menos una letra mayúscula
+    const tieneMayuscula = /[A-Z]/.test(contraseña);
+    // Debe contener al menos un carácter especial
+    const tieneCaracterEspecial = /[!@#$%^&*]/.test(contraseña);
+    // Debe tener al menos 8 caracteres de longitud
+    const tieneLongitudValida = contraseña.length >= 8;
+
+    return tieneNumero && tieneMayuscula && tieneCaracterEspecial && tieneLongitudValida;
+}
+
 async function register() {
     if (window.verificado === true) {
-        const emailInput = document.getElementById('emailR').value
-        const contraseñaInput = document.getElementById('contraseñaR').value
+        const emailInput = document.getElementById('emailR').value;
+        const contraseñaInput = document.getElementById('contraseñaR').value;
 
-        const validar = registerauth(emailInput, contraseñaInput)
+        if (!validarContraseña(contraseñaInput)) {
+            alert("La contraseña debe contener al menos un número, una letra mayúscula, un carácter especial como (!@#$%^&*) y tener al menos 8 caracteres de longitud.");
+            return;
+        }
+
+        const validar = registerauth(emailInput, contraseñaInput);
         const verificar = await validar
 
             .then((verificar) => {
-                alert("El usuario se registro exitosamente..")
+                alert("El usuario se registro exitosamente..");
                 const user = verificar.user;
-                window.location.href = "../index.html"
+                window.location.href = "../index.html";
             })
 
             .catch((error) => {
                 if (error.code === 'auth/email-already-in-use') {
-                    alert("Este correo electrónico ya está en uso. Por favor, utiliza otro.")
+                    alert("Este correo electrónico ya está en uso. Por favor, utiliza otro.");
                 } else if(error.code === 'auth/invalid-email') {
-                    alert("Correo invalido, sigue el formato: ejemplo@example.com")
+                    alert("Correo invalido, sigue el formato: ejemplo@example.com");
                 }else{
-                    alert("Hubo un error, intenta de nuevo más tarde.")
-                    console.error(error)
+                    alert("Hubo un error, intenta de nuevo más tarde.");
+                    console.error(error);
                 }
-            })
+            });
     }
 }
 
