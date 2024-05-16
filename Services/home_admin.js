91bf9e9020badbfbdc478ab918c63d7888edecf6
4ndresFel1pe <164299986+4ndresFel1pe@users.noreply.github.com>
2024-05-16 07:03:12 -0500

    Update home_admin.js

--- Services/home_admin.js
+++ Services/home_admin.js
@@ -10,7 +10,7 @@ async function cerrarsesion(){
 
     .then((comprobar)=>{
         alert('Sesion cerrada')
-        window.location.href='../index.html'
+        window.location.href='index.html'
     })
     .catch((error)=>{
         alert('Sesion no cerrada')
